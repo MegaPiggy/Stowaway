7b5e28105351265ec569090a172d5013ff32fbb3
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-05-24 20:00:17 -0400

    Start with Jellyfish QM riser

---
+++ Components/JellyfishQuantumMoonRiser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Stowaway.Components
+{
+    [RequireComponent(typeof(JellyfishController))]
+    public class JellyfishQuantumMoonRiser : MonoBehaviour
+    {
+        public QuantumOrbit orbit;
+        private JellyfishController _controller;
+        private bool _isLocked;
+
+        public void Start()
+        {
+            _controller = GetComponent<JellyfishController>();
+            GlobalMessenger<OWRigidbody>.AddListener("QuantumMoonChangeState", OnQuantumMoonStateChanged);
+        }
+
+        public void OnDestroy()
+        {
+            GlobalMessenger<OWRigidbody>.RemoveListener("QuantumMoonChangeState", OnQuantumMoonStateChanged);
+        }
+
+        public void Update()
+        {
+            if (_isLocked)
+            {
+                _controller._isRising = true;
+            }
+        }
+
+        public void OnQuantumMoonStateChanged(OWRigidbody qmBody)
+        {
+            if (qmBody == null || orbit == null) return;
+
+            if (orbit._stateIndex == qmBody.GetComponent<QuantumMoon>().GetStateIndex())
+            {
+                _isLocked = true;
+                _controller._isRising = true;
+            }
+            else
+            {
+                _isLocked = false;
+                _controller._isRising = false;
+            }
+        }
+    }
+}
