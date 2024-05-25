582a907c803c72768043c2d68b1628286bf76a6c
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-05-25 02:35:29 -0400

    Adjust so that Cerberus rises only while locked

--- Components/CerberusJellyfish.cs
+++ Components/CerberusJellyfish.cs
@@ -12,43 +12,32 @@ namespace Stowaway.Components
     {
         private OWRigidbody _planetBody;
         private QuantumOrbit _orbit;
-        private bool _hasBeenLocked;
         private float _riseTime = 30;
-        private float _startTime;
-        private float _endTime;
+        private float _currentTime;
+        private float _originalY;
+        private float _risenY;
 
         public void Start()
         {
             _planetBody = this.GetAttachedOWRigidbody().GetOrigParentBody();
             _orbit = _planetBody.GetComponent<QuantumOrbit>();
-            GlobalMessenger<OWRigidbody>.AddListener("QuantumMoonChangeState", OnQuantumMoonStateChanged);
+            _originalY = transform.localPosition.y;
+            _risenY = _originalY + 14;
         }
 
-        public void OnDestroy()
+        public void Update()
         {
-            GlobalMessenger<OWRigidbody>.RemoveListener("QuantumMoonChangeState", OnQuantumMoonStateChanged);
-        }
-
-        public void FixedUpdate()
-        {
-            transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Lerp(-21, -7, GetProgress()), transform.localPosition.z);
+            var quantumMoon = Locator.GetQuantumMoon();
+            if (quantumMoon != null && _orbit._stateIndex == quantumMoon.GetStateIndex())
+            {
+                _currentTime += Time.deltaTime;
+            }
+            transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Lerp(_originalY, _risenY, GetProgress()), transform.localPosition.z);
         }
 
         public float GetProgress()
         {
-            return _hasBeenLocked ? Mathf.Clamp01(Mathf.InverseLerp(_startTime, _endTime, Time.time)) : 0;
-        }
-
-        public void OnQuantumMoonStateChanged(OWRigidbody qmBody)
-        {
-            if (qmBody == null || _orbit == null) return;
-
-            if (_orbit._stateIndex == qmBody.GetComponent<QuantumMoon>().GetStateIndex() && !_hasBeenLocked)
-            {
-                _hasBeenLocked = true;
-                _startTime = Time.time;
-                _endTime = Time.time + _riseTime;
-            }
+            return Mathf.Clamp01(_currentTime/_riseTime);
         }
     }
 }
