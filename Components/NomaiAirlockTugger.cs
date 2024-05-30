55623f02be72c094e332c95ee619fb725cbc05b1
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-05-30 03:56:27 -0400

    Separate airlock and doors + Make door open and close in separate orbits

---
+++ Components/NomaiAirlockTugger.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static NomaiMultiPartDoor;
+
+namespace Stowaway.Components
+{
+	[RequireComponent(typeof(NomaiAirlock), typeof(OverheadDetector))]
+	public class NomaiAirlockTugger : MonoBehaviour
+	{
+		private NomaiAirlock _nomaiAirlock;
+		private NomaiInterfaceOrb _orb;
+		private NomaiInterfaceSlot _openSlot;
+		private NomaiInterfaceSlot _closedSlot;
+		private OverheadDetector _overheadDetector;
+		public bool canOpenAndClose = false;
+
+		public void Start()
+		{
+			_nomaiAirlock = this.GetRequiredComponent<NomaiAirlock>();
+			_orb = _nomaiAirlock._listInterfaceOrb.FirstOrDefault();
+			_openSlot = _orb._slots.FirstOrDefault(slot => slot.gameObject.name.EndsWith("Open"));
+			if (_openSlot == null) _openSlot = _nomaiAirlock._openSwitches.FirstOrDefault();
+			_closedSlot = _orb._slots.FirstOrDefault(slot => slot.gameObject.name.EndsWith("Close"));
+			if (_closedSlot == null) _closedSlot = _nomaiAirlock._closeSwitches.FirstOrDefault();
+			_overheadDetector = this.GetRequiredComponent<OverheadDetector>();
+			_overheadDetector.OnMoonOverhead += OnMoonOverhead;
+			_overheadDetector.OnMoonNoLongerOverhead += OnMoonNoLongerOverhead;
+		}
+
+		public void OnMoonOverhead(OWRigidbody bodyOverhead)
+		{
+			if (canOpenAndClose) Open();
+			else TugToOpen();
+		}
+
+		public void OnMoonNoLongerOverhead(OWRigidbody bodyOverhead)
+		{
+			if (canOpenAndClose) Close();
+			else TugToClose();
+		}
+
+		public float GetDistanceBetweenSlots()
+		{
+			return Vector3.Distance(_openSlot.transform.position, _closedSlot.transform.position);
+		}
+
+		public Vector3 GetPositionTowards(Vector3 a, Vector3 b)
+		{
+			var direction = (b - a).normalized;
+			return (a + ((GetDistanceBetweenSlots() / 2) * direction));
+		}
+
+		private void Open()
+		{
+			if (_orb._belowSand) return;
+			if (_nomaiAirlock._currentRotationState != RotationState.OPEN)
+			{
+				_orb.StartDragFromPosition(_closedSlot.transform.position);
+				_orb.SetTargetPosition(_openSlot.transform.position);
+			}
+		}
+
+		private void Close()
+		{
+			if (_orb._belowSand) return;
+			if (_nomaiAirlock._currentRotationState != RotationState.CLOSED)
+			{
+				_orb.StartDragFromPosition(_openSlot.transform.position);
+				_orb.SetTargetPosition(_closedSlot.transform.position);
+			}
+		}
+
+		private void TugToOpen()
+		{
+			if (_orb._belowSand) return;
+			if (_nomaiAirlock._currentRotationState == RotationState.CLOSED)
+			{
+				_orb.StartDragFromPosition(_orb.transform.position);
+				_orb.SetTargetPosition(GetPositionTowards(_orb.transform.position, _openSlot.transform.position));
+			}
+			else
+			{
+				_orb.StartDragFromPosition(_orb.transform.position);
+				_orb.SetTargetPosition(_openSlot.transform.position);
+			}
+		}
+
+		private void TugToClose()
+		{
+			if (_orb._belowSand) return;
+			if (_nomaiAirlock._currentRotationState == RotationState.OPEN)
+			{
+				_orb.StartDragFromPosition(_orb.transform.position);
+				_orb.SetTargetPosition(GetPositionTowards(_orb.transform.position, _closedSlot.transform.position));
+			}
+			else
+			{
+				_orb.StartDragFromPosition(_orb.transform.position);
+				_orb.SetTargetPosition(_closedSlot.transform.position);
+			}
+		}
+	}
+}
--- Components/NomaiDoorTugger.cs
+++ Components/NomaiDoorTugger.cs
@@ -15,30 +15,17 @@ namespace Stowaway.Components
 		private NomaiInterfaceOrb _orb;
 		private NomaiInterfaceSlot _idleSlot;
 		private NomaiInterfaceSlot _activateSlot;
-		private NomaiInterfaceSlot _openSlot;
-		private NomaiInterfaceSlot _closedSlot;
 		private OverheadDetector _overheadDetector;
 		public bool canOpenAndClose = false;
 
 		public void Start()
 		{
 			_nomaiDoor = this.GetRequiredComponent<NomaiMultiPartDoor>();
-			if (IsCyclable())
-			{
-				_orb = _nomaiDoor._listInterfaceOrb.FirstSecondOrDefault(slot => slot.gameObject.name.EndsWith("Front"));
-				_idleSlot = _orb._slots.FirstSecondOrDefault(slot => slot.gameObject.name.EndsWith("Front"), slot => slot.gameObject.name.StartsWith("Idle"));
-				if (_idleSlot == null) _idleSlot = _nomaiDoor._cycleSwitches.FirstSecondOrDefault(slot => slot.gameObject.name.EndsWith("Front"), slot => slot.gameObject.name.StartsWith("Idle"));
-				_activateSlot = _orb._slots.FirstSecondOrDefault(slot => slot.gameObject.name.EndsWith("Front"), slot => slot.gameObject.name.StartsWith("Activate"));
-				if (_activateSlot == null) _activateSlot = _nomaiDoor._cycleSwitches.FirstSecondOrDefault(slot => slot.gameObject.name.EndsWith("Front"), slot => slot.gameObject.name.StartsWith("Activate"));
-			}
-			else
-			{
-				_orb = _nomaiDoor._listInterfaceOrb.FirstOrDefault();
-				_openSlot = _orb._slots.FirstOrDefault(slot => slot.gameObject.name.EndsWith("Open"));
-				if (_openSlot == null) _openSlot = _nomaiDoor._openSwitches.FirstOrDefault();
-				_closedSlot = _orb._slots.FirstOrDefault(slot => slot.gameObject.name.EndsWith("Close"));
-				if (_closedSlot == null) _closedSlot = _nomaiDoor._closeSwitches.FirstOrDefault();
-			}
+			_orb = _nomaiDoor._listInterfaceOrb.FirstSecondOrDefault(slot => slot.gameObject.name.EndsWith("Front"));
+			_idleSlot = _orb._slots.FirstSecondOrDefault(slot => slot.gameObject.name.EndsWith("Front"), slot => slot.gameObject.name.StartsWith("Idle"));
+			if (_idleSlot == null) _idleSlot = _nomaiDoor._cycleSwitches.FirstSecondOrDefault(slot => slot.gameObject.name.EndsWith("Front"), slot => slot.gameObject.name.StartsWith("Idle"));
+			_activateSlot = _orb._slots.FirstSecondOrDefault(slot => slot.gameObject.name.EndsWith("Front"), slot => slot.gameObject.name.StartsWith("Activate"));
+			if (_activateSlot == null) _activateSlot = _nomaiDoor._cycleSwitches.FirstSecondOrDefault(slot => slot.gameObject.name.EndsWith("Front"), slot => slot.gameObject.name.StartsWith("Activate"));
 			_overheadDetector = this.GetRequiredComponent<OverheadDetector>();
 			_overheadDetector.OnMoonOverhead += OnMoonOverhead;
 			_overheadDetector.OnMoonNoLongerOverhead += OnMoonNoLongerOverhead;
@@ -46,28 +33,19 @@ namespace Stowaway.Components
 
 		public void OnMoonOverhead(OWRigidbody bodyOverhead)
 		{
-			if (canOpenAndClose) Open();
-			else TugToOpen();
+			if (canOpenAndClose) Activate();
+			else TugToActivate();
 		}
 
 		public void OnMoonNoLongerOverhead(OWRigidbody bodyOverhead)
 		{
-			if (canOpenAndClose) Close();
-			else TugToClose();
 		}
 
 		private bool IsCyclable() => _nomaiDoor._cycleSwitches.Length > 0;
 
 		public float GetDistanceBetweenSlots()
 		{
-			if (IsCyclable())
-			{
-				return Vector3.Distance(_activateSlot.transform.position, _idleSlot.transform.position);
-			}
-			else
-			{
-				return Vector3.Distance(_openSlot.transform.position, _closedSlot.transform.position);
-			}
+			return Vector3.Distance(_activateSlot.transform.position, _idleSlot.transform.position);
 		}
 
 		public Vector3 GetPositionTowards(Vector3 a, Vector3 b)
@@ -76,92 +54,18 @@ namespace Stowaway.Components
 			return (a + ((GetDistanceBetweenSlots() / 2) * direction));
 		}
 
-		private void Open()
+		private void Activate()
 		{
 			if (_orb._belowSand) return;
-			if (_nomaiDoor._currentRotationState != RotationState.OPEN)
-			{
-				if (IsCyclable())
-				{
-					_orb.StartDragFromPosition(_idleSlot.transform.position);
-					_orb.SetTargetPosition(_activateSlot.transform.position);
-				}
-				else
-				{
-					_orb.StartDragFromPosition(_closedSlot.transform.position);
-					_orb.SetTargetPosition(_openSlot.transform.position);
-				}
-			}
+			_orb.StartDragFromPosition(_idleSlot.transform.position);
+			_orb.SetTargetPosition(_activateSlot.transform.position);
 		}
 
-		private void Close()
+		private void TugToActivate()
 		{
 			if (_orb._belowSand) return;
-			if (_nomaiDoor._currentRotationState != RotationState.CLOSED)
-			{
-				if (IsCyclable())
-				{
-					_orb.StartDragFromPosition(_idleSlot.transform.position);
-					_orb.SetTargetPosition(_activateSlot.transform.position);
-				}
-				else
-				{
-					_orb.StartDragFromPosition(_openSlot.transform.position);
-					_orb.SetTargetPosition(_closedSlot.transform.position);
-				}
-			}
-		}
-
-		private void TugToOpen()
-		{
-			if (_orb._belowSand) return;
-			if (IsCyclable())
-			{
-				if (_nomaiDoor._currentRotationState != RotationState.OPEN)
-				{
-					_orb.StartDragFromPosition(_orb.transform.position);
-					_orb.SetTargetPosition(GetPositionTowards(_orb.transform.position, _activateSlot.transform.position));
-				}
-			}
-			else
-			{
-				if (_nomaiDoor._currentRotationState == RotationState.CLOSED)
-				{
-					_orb.StartDragFromPosition(_orb.transform.position);
-					_orb.SetTargetPosition(GetPositionTowards(_orb.transform.position, _openSlot.transform.position));
-				}
-				else
-				{
-					_orb.StartDragFromPosition(_orb.transform.position);
-					_orb.SetTargetPosition(_openSlot.transform.position);
-				}
-			}
-		}
-
-		private void TugToClose()
-		{
-			if (_orb._belowSand) return;
-			if (IsCyclable())
-			{
-				if (_nomaiDoor._currentRotationState != RotationState.CLOSED)
-				{
-					_orb.StartDragFromPosition(_orb.transform.position);
-					_orb.SetTargetPosition(GetPositionTowards(_orb.transform.position, _activateSlot.transform.position));
-				}
-			}
-			else
-			{
-				if (_nomaiDoor._currentRotationState == RotationState.OPEN)
-				{
-					_orb.StartDragFromPosition(_orb.transform.position);
-					_orb.SetTargetPosition(GetPositionTowards(_orb.transform.position, _closedSlot.transform.position));
-				}
-				else
-				{
-					_orb.StartDragFromPosition(_orb.transform.position);
-					_orb.SetTargetPosition(_closedSlot.transform.position);
-				}
-			}
+			_orb.StartDragFromPosition(_orb.transform.position);
+			_orb.SetTargetPosition(GetPositionTowards(_orb.transform.position, _activateSlot.transform.position));
 		}
 	}
 }
