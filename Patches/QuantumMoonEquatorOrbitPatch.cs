8affa442483b9c0cd54c4bb75698b89b49dee675
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-06-07 12:32:58 -0400

    merge into one

--- Patches/QuantumMoonEquatorOrbitPatch.cs
+++ Patches/QuantumMoonEquatorOrbitPatch.cs
@@ -12,26 +12,33 @@ namespace Stowaway.Patches
 		[HarmonyPatch(typeof(QuantumMoon), nameof(QuantumMoon.ChangeQuantumState))]
 		public static IEnumerable<CodeInstruction> QuantumMoon_ChangeQuantumState_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
 		{
-			var matcher = new CodeMatcher(instructions, generator);
-			matcher.MatchForward(false,
+			return new CodeMatcher(instructions, generator)
+				.MatchForward(false,
 					new CodeMatch(OpCodes.Ldloc_2),
 					new CodeMatch(OpCodes.Ldc_I4_5)
-				);
-			matcher.Advance(3).CreateLabel(out Label unitY).Advance(-3);
-			matcher.Insert(new CodeInstruction(OpCodes.Ldloc_2), new CodeInstruction(OpCodes.Ldc_I4_3), new CodeInstruction(OpCodes.Beq_S, unitY));
-			matcher.MatchForward(true,
-					new CodeMatch(OpCodes.Conv_R4)
-				);
-			matcher.CreateLabel(out Label calculateOrbitVelocity);
-			matcher.Insert(new CodeInstruction(OpCodes.Br_S, calculateOrbitVelocity), new CodeInstruction(OpCodes.Ldc_I4_0, 0));
-			matcher.Advance(1).CreateLabel(out Label giantsDeepAngle);
-			matcher.Start();
-			matcher.MatchForward(false,
+				).Advance(3).CreateLabel(out Label unitY).Advance(-3)
+				.Insert(
+					new CodeInstruction(OpCodes.Ldloc_2),
+					new CodeInstruction(OpCodes.Ldc_I4_3),
+					new CodeInstruction(OpCodes.Beq_S, unitY),
+					new CodeInstruction(OpCodes.Ldloc_2),
+					new CodeInstruction(OpCodes.Ldc_I4_1),
+					new CodeInstruction(OpCodes.Beq_S, unitY)
+				)
+				.MatchForward(true, new CodeMatch(OpCodes.Conv_R4)).CreateLabel(out Label calculateOrbitVelocity)
+				.Insert(
+					new CodeInstruction(OpCodes.Br_S, calculateOrbitVelocity),
+					new CodeInstruction(OpCodes.Ldc_I4_0, 0)
+				)
+				.Advance(1).CreateLabel(out Label giantsDeepAngle).Start().MatchForward(false,
 					new CodeMatch(OpCodes.Ldc_I4_0),
 					new CodeMatch(OpCodes.Ldc_I4, 360)
-				);
-			matcher.Insert(new CodeInstruction(OpCodes.Ldloc_2), new CodeInstruction(OpCodes.Ldc_I4_3), new CodeInstruction(OpCodes.Beq_S, giantsDeepAngle));
-			return matcher
+				)
+				.Insert(
+					new CodeInstruction(OpCodes.Ldloc_2),
+					new CodeInstruction(OpCodes.Ldc_I4_3),
+					new CodeInstruction(OpCodes.Beq_S, giantsDeepAngle)
+				)
 				.InstructionEnumeration();
 		}
 	}
