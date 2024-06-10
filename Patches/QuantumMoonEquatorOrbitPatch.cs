2075519bd68b739cc4296f4620620af067cf7c1f
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-06-10 12:47:23 -0400

    Add eye state to equator patch
    mobius already starts the qm at the equator, so all i need to do is set inclination to 0 here

--- Patches/QuantumMoonEquatorOrbitPatch.cs
+++ Patches/QuantumMoonEquatorOrbitPatch.cs
@@ -30,14 +30,17 @@ namespace Stowaway.Patches
 					new CodeInstruction(OpCodes.Br_S, calculateOrbitVelocity),
 					new CodeInstruction(OpCodes.Ldc_I4_0, 0)
 				)
-				.Advance(1).CreateLabel(out Label giantsDeepAngle).Start().MatchForward(false,
+				.Advance(1).CreateLabel(out Label zeroAngle).Start().MatchForward(false,
 					new CodeMatch(OpCodes.Ldc_I4_0),
 					new CodeMatch(OpCodes.Ldc_I4, 360)
 				)
 				.Insert(
 					new CodeInstruction(OpCodes.Ldloc_2),
 					new CodeInstruction(OpCodes.Ldc_I4_3),
-					new CodeInstruction(OpCodes.Beq_S, giantsDeepAngle)
+					new CodeInstruction(OpCodes.Beq_S, zeroAngle),
+					new CodeInstruction(OpCodes.Ldloc_2),
+					new CodeInstruction(OpCodes.Ldc_I4_5),
+					new CodeInstruction(OpCodes.Beq_S, zeroAngle)
 				)
 				.InstructionEnumeration();
 		}
