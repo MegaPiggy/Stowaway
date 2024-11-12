2629b831a8140122c5baa15c7c94a17d6472fcb3
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-11-12 01:50:53 -0500

    start on ghost matter float

---
+++ Components/GhostMatterFloatController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Stowaway.Components
+{
+	[RequireComponent(typeof(OverheadDetector))]
+	public class GhostMatterFloatController : MonoBehaviour
+	{
+		private OverheadDetector _overheadDetector;
+
+		private void Start()
+		{
+			_overheadDetector = this.GetRequiredComponent<OverheadDetector>();
+		}
+	}
+}
