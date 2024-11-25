5a7d140d5b42d5108c8490ece9223e0cd447bd3a
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-11-24 20:27:13 -0500

    i think this is a good start even if it doesn't do anything yet lol

--- Components/GhostMatterFloatController.cs
+++ Components/GhostMatterFloatController.cs
@@ -12,9 +12,31 @@ namespace Stowaway.Components
 	{
 		private OverheadDetector _overheadDetector;
 
-		private void Start()
+		public void Start()
 		{
 			_overheadDetector = this.GetRequiredComponent<OverheadDetector>();
+		}
+
+		public void OnDestroy()
+		{
+		}
+
+		public void Update()
+		{
+			var isMoonOverhead = _overheadDetector.IsMoonOverhead();
+			var isSunOverhead = _overheadDetector.IsSunOverhead();
+
+			if (isMoonOverhead || isSunOverhead) // stretches
+			{
+				var bothFactor = Mathf.Clamp01(_overheadDetector.GetMoonOverheadPercentage() * _overheadDetector.GetSunOverheadPercentage());
+
+			}
+
+			if (isMoonOverhead) // moves
+			{
+				// gradually rises during alignment, then falls back to starting position.
+				var moonFactor = _overheadDetector.GetMoonOverheadPercentage();
+			}
 		}
 	}
 }
